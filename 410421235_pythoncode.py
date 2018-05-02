e9833ee9088ba45eeac85d3d1351e754c29ff311
paul1015 <chungpaul1015@gmail.com>
2018-05-02 19:36:18 +0800

    /Users/paul1015/Documents/GitHub/lastlocal/410421235_pythoncode.py

---
+++ 410421235_pythoncode.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import numpy as np
+import imageio as im
+dataI = Image.open("I.png")
+dataE = Image.open("E.png")
+dataEprime = Image.open("Eprime.png")
+datakey1 = Image.open("key1.png")
+datakey2 = Image.open("key2.png")
+
