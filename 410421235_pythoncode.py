2db81fcb13fb94331a161c4ec9cb253224818b61
paul1015 <chungpaul1015@gmail.com>
2018-05-02 19:38:03 +0800

    /Users/paul1015/Documents/GitHub/lastlocal/410421235_pythoncode.py

--- 410421235_pythoncode.py
+++ 410421235_pythoncode.py
@@ -7,3 +7,13 @@ dataEprime = Image.open("Eprime.png")
 datakey1 = Image.open("key1.png")
 datakey2 = Image.open("key2.png")
 
+data = np.zeros((120000,3),int)
+e = np.zeros((120000), int)
+w, h = datakey1.size
+for i in range(h):
+    for j in range(w):
+        data[i*400+j][0] = datakey1.getpixel((j,i))
+        data[i*400+j][1] = datakey2.getpixel((j,i))
+        data[i*400+j][2] = dataI.getpixel((j,i))
+        e[i*400+j] = dataEprime.getpixel((j,i))
+
